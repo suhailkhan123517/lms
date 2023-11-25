533ee1ebaec0a6247b9d7257dda266c72114941e
Mohammad suhail <skhan282290@gmail.com>
2023-11-25 18:47:44 +0530

    saturday

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
   return (
     <>
-      <div>
-        <h1>Courses</h1>
+      <div className="p-6">
+        <Link href="/teacher/create">
+          <Button>New Course</Button>
+        </Link>
       </div>
     </>
   );
